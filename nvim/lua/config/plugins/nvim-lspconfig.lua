344e02e6384b2306cef31b7b2a7f354af3113362
Vadim Zyamalov <zyamalov.ve@yandex.ru>
2025-05-09 19:38:05 +0300

    Еще повозимся

--- nvim/lua/config/plugins/nvim-lspconfig.lua
+++ nvim/lua/config/plugins/nvim-lspconfig.lua
@@ -2,7 +2,7 @@ return {
   -- nvim-lspconfig - встроенный набор настроек для работы LSP с разными языками.
   {
     "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
+    event = { "FileType" },
     config = function()
       vim.api.nvim_create_autocmd("LspAttach", {
         group = vim.api.nvim_create_augroup("my.lsp", {}),

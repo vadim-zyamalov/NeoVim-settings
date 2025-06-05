24e9c39e256ba5e29f6de142ada22b5e531974c1
Vadim Zyamalov <zyamalov.ve@yandex.ru>
2025-06-05 23:19:07 +0300

    Отныне сидеть на дереве будем на разных ветках

--- nvim/lua/config/plugins/nvim-treesitter.lua
+++ nvim/lua/config/plugins/nvim-treesitter.lua
@@ -1,24 +1,40 @@
+local nv11 = vim.fn.has("nvim-0.11")
+local parsers = { "lua", "python", "latex" }
+
 return {
   -- Treesitter. Сложно сказать, куда его включать...
   -- Это и жнец, и швец, и на дуде игрец.
 
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  branch = "main",
-  cond = (vim.fn.has("nvim-0.11") == 1),
+  branch = nv11 == 1 and "main" or "master",
   lazy = false,
   config = function()
-    local treesitter = require("nvim-treesitter")
-    treesitter.setup {
-      sync_install = false,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = {
-        enable = true,
-      },
-    }
-    treesitter.install { "lua", "python", "latex" }
+    if nv11 == 1 then
+      local treesitter = require("nvim-treesitter")
+      treesitter.install(parsers)
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = parsers,
+        callback = function()
+          vim.treesitter.start()
+          vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
+    else
+      local treesitter = require("nvim-treesitter.configs")
+      treesitter.setup {
+        ensure_installed = parsers,
+        sync_install = false,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        indent = {
+          enable = true,
+        },
+      }
+    end
   end,
 }
